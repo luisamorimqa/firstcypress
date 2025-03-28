fbf81549ee58096e4cbc5ec42abc855198ab0c33
luisamorimqa <luis.amorim.qa@gmail.com>
2025-03-28 14:04:55 -0300

    Dashboard de execuções

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'od1pii',
   e2e: {
     baseUrl: 'https://automationpratice.com.br/',
     reporter: 'cypress-mochawesome-reporter',
-    reportPageTitle: 'Cypress Test Results',
-    charts: true,
+    reporterOptions: {
+      reportTitle: 'Cypress Test Results',
+      reportPageTitle: 'Cypress Test Results',
+      charts: true,
+    },
+    defaultCommandTimeout: 5000,
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
