a872ea9f23487fc723979c55be7015b0e273bc5e
luisamorimqa <luis.amorim.qa@gmail.com>
2025-03-28 11:29:34 -0300

    Implementacao de testes de cadastro de usuário

--- cypress/e2e/cadastro_usuario.cy.js
+++ cypress/e2e/cadastro_usuario.cy.js
@@ -1,5 +1,12 @@
+import { faker } from '@faker-js/faker'
 import home_page from '../support/pages/home_page'
 import cadastro_usuario from '../support/pages/cadastro_usuario_page'
+
+const user = {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+}
 
 describe('Cadastro de usuário', () => {
 
@@ -13,22 +20,40 @@ describe('Cadastro de usuário', () => {
     })
 
     it('Cadastro de usuário sem informar email', () => {
-
+        cadastro_usuario.fillName(user.name)
+        cadastro_usuario.fillPassword(user.password)
+        cadastro_usuario.clickRegister()
+        cadastro_usuario.checkErrorMessage('O campo e-mail deve ser prenchido corretamente')
     })
 
     it('Cadastro de usuário informando email inválido', () => {
-
+        cadastro_usuario.fillName(user.name)
+        cadastro_usuario.fillEmail('email.com')
+        cadastro_usuario.fillPassword(user.password)
+        cadastro_usuario.clickRegister()
+        cadastro_usuario.checkErrorMessage('O campo e-mail deve ser prenchido corretamente')
     })
 
     it('Cadastro de usuário sem informar senha', () => {
-
+        cadastro_usuario.fillName(user.name)
+        cadastro_usuario.fillEmail(user.email)
+        cadastro_usuario.clickRegister()
+        cadastro_usuario.checkErrorMessage('O campo senha deve ter pelo menos 6 dígitos')
     })
 
     it('Cadastro de usuário informando senha inválida', () => {
-
+        cadastro_usuario.fillName(user.name)
+        cadastro_usuario.fillEmail(user.email)
+        cadastro_usuario.fillPassword('123')
+        cadastro_usuario.clickRegister()
+        cadastro_usuario.checkErrorMessage('O campo senha deve ter pelo menos 6 dígitos')
     })
 
     it('Cadastro de usuário com sucesso', () => {
-
+        cadastro_usuario.fillName(user.name)
+        cadastro_usuario.fillEmail(user.email)
+        cadastro_usuario.fillPassword(user.password)
+        cadastro_usuario.clickRegister()
+        cadastro_usuario.checkSuccessMessage(user.name)
     })
 })
