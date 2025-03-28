199d49527896e4e2d57a97ffef02600a9e8cec06
luisamorimqa <luis.amorim.qa@gmail.com>
2025-03-28 13:42:44 -0300

    Adição do Mockawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://automationpratice.com.br/',
+    reporter: 'cypress-mochawesome-reporter',
+    reportPageTitle: 'Cypress Test Results',
+    charts: true,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
