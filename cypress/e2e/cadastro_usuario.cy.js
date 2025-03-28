395d4be6f51491348aa55abff058d262119e6a5a
luisamorimqa <luis.amorim.qa@gmail.com>
2025-03-27 22:08:07 -0300

    Criando projeto

---
+++ cypress/e2e/cadastro_usuario.cy.js
@@ -0,0 +1,25 @@
+describe('Cadastro de usuário', () => {
+    it('Cadastro de usuário sem informar nome', () => {
+
+    })
+
+    it('Cadastro de usuário sem informar email', () => {
+
+    })
+
+    it('Cadastro de usuário informando email inválido', () => {
+
+    })
+
+    it('Cadastro de usuário sem informar senha', () => {
+
+    })
+
+    it('Cadastro de usuário informando senha inválida', () => {
+
+    })
+
+    it('Cadastro de usuário com sucesso', () => {
+
+    })
+})
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,30 @@
+describe('Login', () => {
+
+    it('Login sem informar email', () => {
+
+    })
+
+    it('Login sem informar senha', () => {
+        
+    })
+
+    it('Login sem informar email e senha'   , () => {
+        
+    })
+
+    it('Login informando email invalido', () => {
+        
+    })
+
+    it('Login informando senha invalida', () => {
+        
+    })
+
+    it('Login informando email e senha invalidos', () => {
+        
+    })
+
+    it('Login com sucesso', () => {
+
+    })
+})
